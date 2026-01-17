fd42690fbb9f3cb03eee8e82be5646eb40e910a1
progFilthi <your-email@example.com>
2026-01-18 00:07:57 +0200

    added swagger docs and changed controller to restcontroller within presigned controller

--- src/main/java/com/progfilthi/awss3/controllers/PresignedUrlController.java
+++ src/main/java/com/progfilthi/awss3/controllers/PresignedUrlController.java
@@ -6,12 +6,12 @@ import com.progfilthi.awss3.services.PresignedUrlService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/s3")
 @RequiredArgsConstructor
 public class PresignedUrlController {
